f8327ccb73dfb73665578f2d5731faa968b447f5
abhi <abhilearnings2005@gmail.com>
2021-02-15 00:22:55 -0500

    test

---
+++ JenkinsPOMTest/src/test/java/test1/com/TestJenkins.java
@@ -0,0 +1,17 @@
+package test1.com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+	
+	@Test
+	public void invokebrowser(){
+		
+		System.setProperty("webdriver.chrome.driver", ".//BrowsersDrivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://cookbook.seleniumacademy.com/bmicalculator.html");
+	}
+
+}
